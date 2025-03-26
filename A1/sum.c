a82b3a2ad70ea965621fd174d5c881aeffc17b88
Ultrafaffe <90902427+Ultrafaffe@users.noreply.github.com>
2025-03-26 17:07:27 +0100

    start

--- A1/sum.c
+++ A1/sum.c
@@ -1,17 +1,63 @@
-#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
 
+int main(int argc, char **argv)
+{
+	int rank, num_processes;
+	int local_number;
+	int local_sum, global_sum;
 
-int main(int argc, char **argv) {
-	if (2 != argc) {
-		printf("Usage: sum number_of_summands\n");
-		return 1;
+	// Initialize MPI
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+
+	// Seed random number generator differently for each process
+	srand(time(NULL) * (rank + 1));
+
+	// Each process generates its own random local number
+	local_number = rand() % 100; // Random number between 0-99
+	local_sum = local_number;
+
+	printf("Process %d: Local number = %d\n", rank, local_number);
+
+	// Tree-structured reduction
+	for (int step = 1; step < num_processes; step *= 2)
+	{
+		// Calculate partner process
+		int partner;
+		if (rank % (2 * step) == 0)
+		{
+			// Receiving process
+			partner = rank + step;
+			if (partner < num_processes)
+			{
+				int received_sum;
+				MPI_Recv(&received_sum, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				local_sum += received_sum;
+			}
+		}
+		else
+		{
+			printf("Process %d: step = %d\n", rank, step);
+			// Sending process
+			partner = rank - step;
+			MPI_Send(&local_sum, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+			break; // Sending process stops after sending
+		}
 	}
-	int num_steps = atoi(argv[1]);
 
+	// Root (process 0) prints global sum
+	if (rank == 0)
+	{
+		global_sum = local_sum;
+		printf("\nGlobal Sum: %d\n", global_sum);
+	}
 
-	printf("Parallel Summation\n");
+	// Finalize MPI
+	MPI_Finalize();
 
 	return 0;
 }
---
+++ A1/treeSum.c
@@ -0,0 +1,72 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE 9
+
+int array[ARRAY_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int main(int argc, char **argv)
+{
+    int rank, size;
+    int sum = 0;
+    int received;
+    int num;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    // printf("Size is %d\n", size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // int num = rank; //+ rand() % 100;
+    int sizeOfArray = sizeof(array) / sizeof(array[0]);
+    int block = sizeOfArray / size;
+    int start = rank * block;
+    int end = (sizeOfArray % size != 0 && rank == (size - 1)) ? (start + block + (sizeOfArray % size) - 1) : start + block;
+
+    for (int j = start; j <= end; j++)
+    {
+        sum += array[j];
+    }
+
+    // for(int ö=0; ö<ARRAY_SIZE; ö++){
+    //     printf("Array %d\n", array[ö]);
+    // }
+    // int num = array[rank];
+
+    // sum = num;
+    num = sum;
+    // printf("Rank %d STARTS WITH %d\n", rank, sum);
+    for (int div = 1; div < size; div *= 2)
+    {
+        int partner;
+
+        if (rank % (div * 2) == 0)
+        {
+            partner = rank + div;
+            if (partner < size) // Check if the rank is not the last one
+            {
+                MPI_Recv(&received, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                num += received;
+                printf("Rank %d received %d from Rank %d\n", rank, sum, partner);
+            }
+        }
+        else // will never be rank 0, send to partner depending on the div
+        {
+            partner = rank - div;
+            printf("I: %d WANT TO SEND %d\n", rank, num);
+            MPI_Send(&num, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+            break;
+        }
+    }
+
+    if (rank == 0)
+    {
+        sum = num;
+        printf("Sum is %d\n", sum);
+    }
+
+    MPI_Finalize();
+
+    return 0;
+}

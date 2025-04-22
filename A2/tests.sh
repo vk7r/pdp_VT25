c2d1846b1d7ddc30385276428a130e15236c7abe
vk7r <viktorkangasniemi@gmail.com>
2025-04-22 13:00:18 +0200

    updated script for weak scaling

--- A2/tests.sh
+++ A2/tests.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 echo "=== STRONG SCALING ==="
-for P in 1 2 4 ; do
+for P in 1 2 4 8 16; do
     echo "--- $P process(es) ---"
-    mpirun --bind-to none -np $P ./stencil input96.txt output96.txt 1 
+    mpirun --bind-to none -np $P ./stencil input96.txt output_scaling.txt 4
 done
 
 echo "=== WEAK SCALING ==="
-for P in 1 2 4 ; do
+# Input files for increasing problem sizes
+input_files=("input96.txt" "input1000000.txt" "input2000000.txt" "input4000000.txt" "input8000000.txt")
+
+i=0
+for P in 1 2 4 8 16; do
     echo "--- $P process(es) ---"
-    mpirun --bind-to none -np $P ./stencil input96.txt output96.txt $P 
+    mpirun --bind-to none -np $P ./stencil "${input_files[$i]}" output_scaling.txt 4
+    ((i++))
 done
